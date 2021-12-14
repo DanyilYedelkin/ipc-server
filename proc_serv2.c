c780239da24fed1115cf1646dd9d8e1e79a851f2
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2021-12-14 19:18:11 +0100

    Update proc_serv2.c (added comments and fixed some bugs)

--- proc_serv2.c
+++ proc_serv2.c
@@ -1,62 +1,5 @@
-/*#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-void error(char *msg);
-
-int main(int argc, char *argv[]){
-    int sock, length, fromlen, n;
-    struct sockaddr_in server;
-    struct sockaddr_in from;
-    char buf[1024];
-    from.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if(argc < 2){
-        fprintf(stderr, "ERROR, no port provided\n");
-        exit(0);
-    }
-
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sock < 0){
-        error("Opening socket");
-    }
-    length = sizeof(server);
-    bzero(&server, length);
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(atoi(argv[1]));
-    if(bind(sock, (struct sockaddr *)&server, length) < 0){
-        error("blinding");
-    }
-    fromlen = sizeof(struct sockaddr_in);
-
-    while(1){
-        n = recvfrom(sock, buf, 1024, 0, (struct sockaddr *)&from, &fromlen);
-        if(n < 0){
-            error("recvfrom");
-        }
-        write(1, "Received a datagram: ", 21);
-        write(1, buf, n);
-        n = sendto(sock, "Got your message\n", 17, 0, (struct sockaddr *)&from, fromlen);
-        if(n < 0){
-            error("sendto");
-        }
-    }
-
-    return 0;
-}
-
-void error(char *msg){
-    perror(msg);
-    exit(0);
-}*/
-
-
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> //for atoi()
 #include <fcntl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -68,8 +11,91 @@ void error(char *msg){
 #include <unistd.h>
 
 
-int getch(void)
-{
+
+int getch(void);
+void error(char *errorMessage);
+
+//the main function
+int main(int argc, char* argv[]){
+    //checks if input arguments are less, or more than 2 => returns the error message
+    if(argc != 2){ 
+        error("Error: Incorrect number of input arguments\n"); 
+    }
+
+    int firstPort = atoi(argv[1]);  //converts the string argument argv[1] to an integer (firstPort)
+    //if firstPort is 0, than we will have a mistake (error message)
+    if(firstPort == 0){ //checks, if converting has a mistake
+        error("Error: Input argument isn't number\n");  //return the mistake message
+    }
+
+    int firstSocket = socket(AF_INET, SOCK_DGRAM, 0);   // function shall create an unbound socket in a communications domain
+    //AF_INET (AF - Address Family) -  communication domain, refers to addresses from the internet, IP addresses specifically
+    //SOCK_DGRAM - Provides datagrams, which are connectionless-mode, unreliable messages of fixed maximum length.
+
+    if(firstSocket == -1){ //if we can't create a socket, than returns error message
+        error("Error in creating a socket\n"); 
+    }
+
+    //open the file "serv2.txt", with flags and 0777 mod
+    int file = open("serv2.txt", O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0777);
+    if(file == -1){ //if we can't open the file, than it returns error message
+        error("Error in opening the file\n"); 
+    }
+
+
+    struct sockaddr_in client;  //the basic structure for all syscalls and functions that deal with internet addresses
+    // Filling server information
+
+    /*      Information about "struct sockaddr_in" and "struct in_addr"
+    struct sockaddr_in {
+        short            sin_family;   // e.g. AF_INET
+        unsigned short   sin_port;     // e.g. htons(3490)
+        struct in_addr   sin_addr;     // see struct in_addr, below
+        char             sin_zero[8];  // zero this if you want to
+    };
+
+    struct in_addr {
+        unsigned long s_addr;  // load with inet_aton()
+    };
+    */
+
+    client.sin_family = AF_INET; //IPv4 protocol
+    // inet_aton("127.0.0.1", &client.sin_addr.s_addr); or we can write like this:
+    //function shall convert the string pointed to by cp, in the standard IPv4 dotted decimal notation, to an integer value suitable for use as an Internet address
+    client.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    client.sin_port = htons(firstPort); //translates a short integer from host byte order to network byte order
+
+    //assigns the address specified by &client to the socket referred to by the file descriptor firstSocket. 
+    // sizeOfSocket specifies the size, in bytes, of the address structure pointed to by client
+    socklen_t sizeOfSocket = sizeof(struct sockaddr);
+    if(bind(firstSocket, (struct sockaddr*) &client, sizeOfSocket) == -1){
+        error("Error in bind\n");   //if it has an error, than it returns the error message
+    }
+    //kill(getppid(),SIGUSR1);
+
+    char buffer[4096];  //create a buffer for read and write the file's content
+
+    int element, input;
+    while((element = read(firstSocket, buffer, 4096)) > 0){
+        write(file, buffer, element);
+    }
+
+    //the exit of the program
+    close(firstSocket);
+    exit(EXIT_SUCCESS);
+
+    return 0;
+}
+
+// a method for an error message and exit with failure 
+void error(char *errorMessage){
+    fprintf(stderr,"[./proc_serv2] %s", errorMessage);
+
+    exit(EXIT_FAILURE);
+}
+
+//create this method for having a mistake with declaration method in the code (old version of the code)
+int getch(void){
     struct termios oldattr, newattr;
     int ch;
     tcgetattr( STDIN_FILENO, &oldattr );
@@ -79,64 +105,4 @@ int getch(void)
     ch = getchar();
     tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
     return ch;
-}
-
-
-#define BUFFER 1024 * 4
-
-void error(char *error);
-
-int main(int argc, char* argv[]) {
-
-    if(argc != 2) { error("incorrect number of input arguments \n"); }
-
-    int port1 = atoi(argv[1]);
-    if(port1 == 0) { error("input argument isn't number \n"); }
-
-    struct sockaddr_in client;
-
-    int socket1 = socket(AF_INET, SOCK_DGRAM, 0);
-    if(socket < 0) { error("error socket\n"); }
-
-    int file = open("serv2.txt", O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0777);
-    if(file < 0) { error("error txt file \n"); }
-
-    // Filling server information
-    client.sin_family = AF_INET; //IpV4;
-    client.sin_port = htons(port1);
-    client.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if(bind(socket1, (struct sockaddr*) &client, sizeof(struct sockaddr)) < 0) {
-        error("error bind\n");
-    }
-    //kill(getppid(),SIGUSR1);
-
-    char str[BUFFER];
-    //socklen_t size = sizeof(struct sockaddr);
-
-    /*for(int i = 0; i < 20; i++) {
-        if((recv(socket1, str, BUFFER, 0)) < 0) {
-            error("error recv");
-        }
-        write(file, str, BUFFER);
-    }*/
-
-    int c, input;
-    while((c = read(socket1, str, BUFFER)) > 0) {
-        write(file, str, c);
-    }
-
-    /*if(recvfrom(socket1, str, BUFFER, 0, (struct sockaddr*) &server, &size) < 0) {
-        error("error recvfrom");
-    }*/
-
-    //kill(getppid(),SIGUSR2);
-
-    close(socket1);
-    exit(EXIT_SUCCESS);
-}
-
-void error(char *error) {
-    fprintf(stderr,"[./proc_serv2] %s", error);
-    exit(EXIT_FAILURE);
 }

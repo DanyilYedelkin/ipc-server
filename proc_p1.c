1fa2c5fb173eb6a6c57efa53503192ae7d09f0e6
Danyil Yedelkin <danyil.yedelkin@student.tuke.sk>
2021-12-16 19:05:01 +0100

    Update proc_p1.c (add comments and fixed something)

--- proc_p1.c
+++ proc_p1.c
@@ -18,12 +18,12 @@ void writePipe()
 {
 	char *buffer = NULL;
 	buffer = calloc(len, sizeof(char));
-	ssize_t readLine = getline(&buffer, &len, fd);
+	ssize_t readLine = getline(&buffer, &len, fd);	//read a delimited record from fd (FILE*)
 	ssize_t writeLine = write(output, buffer, strlen(buffer));
 	if (readLine == -1 || writeLine == -1)
 	{
-		printf("With read||write pipe proc_p1\n");
-		exit(1);
+		printf("With read || write pipe proc_p1\n");	//error message
+		exit(EXIT_FAILURE);	//exit with failure
 	}
 	printf("P1: SUCCESSFUL \n");
 	free(buffer);
@@ -37,22 +37,25 @@ void killPipe()
 }
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
+	if (argc < 3)	//check number of parameters
 	{
-		printf("Usage %s <ppid> <pipe[0]>\n", argv[0]);
-		exit(1);
+		printf("Usage %s <ppid> <pipe[0]>\n", argv[0]);	//error message
+		exit(EXIT_FAILURE);	//failure exit
 	}
+	//The kill() function sends a signal to a process or process group specified by pid (getppid()).
+    //The kill() function is successful if the process has permission to send the signal sig(SIGUSR1) to any of the processes specified by pid (getppid()). 
+    //If kill() is not successful, no signal is sent.
 	kill(getppid(), SIGUSR1);
-	output = atoi(argv[1]);
-	if ((fd = fopen("p1.txt", "r")) == NULL)
+	output = atoi(argv[1]);		//write the pipe, atoi converts to int 
+	if ((fd = fopen("p1.txt", "r")) == NULL)	//open the file and checks it
 	{
-		perror("p1.txt wasn't opened!\n");
-		exit(EXIT_FAILURE);
+		perror("p1.txt wasn't opened!\n");	//error message
+		exit(EXIT_FAILURE);	//failure exit
 	}
-	printf("Signal Send to write \n");
-	signal(SIGUSR1, writePipe);
-	signal(SIGUSR2, killPipe);
-	printf("END proc_p1\n");
+	printf("Signal Send to write \n");	//write current situation
+	signal(SIGUSR1, writePipe); //sets the disposition of the signal SIGUSR1 to writePipe
+	signal(SIGUSR2, killPipe); //sets the disposition of the signal SIGUSR2 to killPipe
+	printf("END proc_p1\n");	//write current situation
 	while (1)
 	{
         pause();
